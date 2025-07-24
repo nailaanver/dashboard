6ec30690dc0dcc4b366ef1199e0e523bbf3aeb6f
Fathima Naila <fathimanaila71@gmail.com>
2025-07-24 16:31:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// counter
+document.addEventListener('aos:in', ({ detail }) => {
+  console.log('animated in', detail);
+});
+
+document.addEventListener('aos:out', ({ detail }) => {
+  console.log('animated out', detail);
+});
